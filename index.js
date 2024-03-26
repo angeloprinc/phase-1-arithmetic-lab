8b541ef330080728465c9d089d047e5737eebd59
ANGELO PRINCE <angelo@angelo.com>
2024-03-26 09:06:40 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
+// Multiply
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+// Random
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Modulus
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+// Max
+
+let max = Math.max(1, 10, 20);
+
+
+
+
+
+
+
+
+
+   // Write your solution in this file!
+
+var customerName = "bob";
+
+// ----------------------------------------
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+};
+
+// ----------------------------------------
+
+var bestCustomer;
+
+function setBestCustomer(){
+  bestCustomer = "not bob";
+};
+
+// ----------------------------------------
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+};
+
+// ----------------------------------------
+
+const leastFavoriteCustomer = "";
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 1;
+};
